7ea6bd075525f5bf07ff31202e755f64bbeda0b7
beavurus <frederic.saddi@gmail.com>
2019-03-15 15:30:56 -0400

    Added layout variable to Entity.java

--- src/GameObjects/Entity/Entity.java
+++ src/GameObjects/Entity/Entity.java
@@ -4,7 +4,17 @@ import GameObjects.GameObject;
 
 public abstract class Entity extends GameObject {
 
+    private static Entity[][] layout;
+
     public Entity(int posX, int posY, char appearance) {
         super(posX, posY, appearance);
+    }
+
+    public static void setLayout(Entity[][] layout) {
+        Entity.layout = layout;
+    }
+
+    public static Entity[][] getLayout() {
+        return layout;
     }
 }
