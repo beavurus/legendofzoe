916bcd6cf3b16cb2144895520762315fb84796a1
beavurus <frederic.saddi@gmail.com>
2019-03-16 13:11:37 -0400

    Optimisations dans main

--- src/LegendOfZoe.java
+++ src/LegendOfZoe.java
@@ -11,27 +11,27 @@ public class LegendOfZoe {
 
         Messages.afficherIntro();
 
-        GameController gameController = new GameController();
-        gameController.nextLevel();
-        gameController.render();
-        Scanner s = new Scanner(System.in);
-        gameController.tourZoe(s.nextLine().charAt(0));
-        gameController.tourMonstres();
-        gameController.render();
-
-//        Messages.afficherIntro();
-//
-//        GameController controller = new GameController();
+//        GameController gameController = new GameController();
+//        gameController.nextLevel();
+//        gameController.render();
 //        Scanner s = new Scanner(System.in);
-//
-//        while (true) {
-//            String input = s.nextLine();
-//            controller.render();
-//            for (int i = 0; i < input.length(); i++) {
-//                controller.tourZoe((input.charAt(i)));
-//                controller.tourMonstres();
-//            }
-//        }
+//        gameController.tourZoe(s.nextLine().charAt(0));
+//        gameController.tourMonstres();
+//        gameController.render();
+
+        GameController controller = new GameController();
+        Scanner s = new Scanner(System.in);
+        controller.nextLevel();
+        controller.render();
+        while (true) {
+            String input = s.nextLine();
+            char[] commandes = input.toCharArray();
+            for (char c : commandes) {
+                controller.tourZoe(c);
+                controller.tourMonstres();
+            }
+            controller.render();
+        }
 
     }
 }
